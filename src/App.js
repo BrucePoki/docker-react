8dc54e43072b7e22765259d29a57e2b8a9e53a47
jiangzhengtao <jiangzhengtao@baidu.com>
2022-04-25 21:55:02 +0800

    initial  commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WOW! This is so so so cool...<br></br>
+          Let's goooooooo!
         </p>
         <a
           className="App-link"
